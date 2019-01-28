2ac54b2558f1761ee8897839aea11653c42c4813
Monicam02 <mailme.mmonica@gmail.com>
2019-01-28 10:43:41 +0530

    first commit1

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {RoboComponent} from './robo.component'
+import { ProductComponent } from './products/products.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RoboComponent,
+    ProductComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/products/products.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector: 'product-sri',
+    templateUrl: './products.component.html',
+    
+  })
+
+  export class ProductComponent{
+    
+
+onInit(){
+let title = "### Sri Product List";
+}
+  }
